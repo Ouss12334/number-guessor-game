5cd5d5dcf0c0529f2ab87fb7cb141129bdf17cf5
ItsMEOT <oussama.turki@value.com.tn>
2021-05-08 12:07:08 +0100

    init

---
+++ src/main/java/com/generator/randomgeneratorgame/RandomGeneratorGameApplication.java
@@ -0,0 +1,13 @@
+package com.generator.randomgeneratorgame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RandomGeneratorGameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RandomGeneratorGameApplication.class, args);
+	}
+
+}
